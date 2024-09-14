f9ff383ba33210aa81f47e641cca05310cd34216
TharushaK01 <tharushakavinda01@gmail.com>
2024-09-14 09:02:09 +0530

    Small error

---
+++ src/app/shared/todo.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TodoService {
+  firestoreCollection : AngularFirestoreCollection;
+
+  constructor(private firestore: AngularFirestore) {
+    this.firestoreCollection = firestore.collection('todos');
+   }
+
+   addTodo(title: string) {
+    this.firestoreCollection.add({
+      title,
+      isDone : false
+    })
+   }
+}
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+   production: true,
+   firebaseConfig: {
+    apiKey: "AIzaSyAuL7hM6gmx3lqAutTQHEiA-U1_NTRJntI",
+    authDomain: "todo-app-a1e10.firebaseapp.com",
+    projectId: "todo-app-a1e10",
+    storageBucket: "todo-app-a1e10.appspot.com",
+    messagingSenderId: "126469523950",
+    appId: "1:126469523950:web:335198e05881b09c83e6ff"
+  } 
+};
